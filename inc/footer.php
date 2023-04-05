a571e0d8eddd0c03d22ee4ae0fa925ade2581a13
Jakub Hošek <hosj03@vse.cz>
2023-04-05 17:11:28 +0200

    footer to bottom of page

--- inc/footer.php
+++ inc/footer.php
@@ -1,16 +1,12 @@
-<?php
-echo '<footer>
+<div class="spacer"></div>
+<footer>
     <p>
         Author: Jakub Hošek (hosj03)<br>
     </p>
     <p>
-        This page was created as bachelor\'s work at Prague University of Economics and Business. If you have problem with the shared content, contact at hosj03@vse.cz.
+        This page was created as bachelor's work at Prague University of Economics and Business. If you have problem with the shared content, contact at hosj03@vse.cz.
     </p>
 </footer>
    <script src="/resources/main.js"></script>
 </body>
-</html>';
-
-//    <p>
-//        Text is available under the <a href="https://creativecommons.org/licenses/by-sa/3.0/" class="cc">Creative Commons Attribution-ShareAlike License</a>.
-//    </p>
+</html>
